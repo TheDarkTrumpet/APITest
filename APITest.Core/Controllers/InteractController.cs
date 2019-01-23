8dbe68b186a85eb5b390483ebdfe58712feb9981
David Thole <dthole@gmail.com>
2019-01-23 14:04:33 -0600

    Making the post require a JSON Object, and just outputting it

--- APITest.Core/Controllers/InteractController.cs
+++ APITest.Core/Controllers/InteractController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace APITest.Core.Controllers
 {
@@ -14,6 +15,12 @@ namespace APITest.Core.Controllers
         private static readonly log4net.ILog log =
             log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Stub method";
+        }
+
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
@@ -23,7 +30,7 @@ namespace APITest.Core.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] JObject value)
         {
             log.Info("Received POST: " + value);
         }
