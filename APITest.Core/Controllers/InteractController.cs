0e079c2c3673d55d53bcb666c32c561637fc854c
David Thole <dthole@gmail.com>
2019-01-23 12:40:31 -0600

    Changing the log4net location, the stubs are simple, using log4net to log the reqests

--- APITest.Core/Controllers/InteractController.cs
+++ APITest.Core/Controllers/InteractController.cs
@@ -11,17 +11,21 @@ namespace APITest.Core.Controllers
     [ApiController]
     public class InteractController : ControllerBase
     {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            log.Info("ID Request Received: " + id);
+            return id.ToString();
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
-
+            log.Info("Received POST: " + value);
         }
     }
 }
