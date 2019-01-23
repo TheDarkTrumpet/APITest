b1264ca47af02bbc5ddac9f3138ca82f1a6a2200
David Thole <dthole@gmail.com>
2019-01-23 12:26:03 -0600

    Adding log4net, and adding a new controller - one to get the current log file (incase not pulling from docker), and the other to interact with the API

---
+++ APITest.Core/Controllers/InteractController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITest.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InteractController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+    }
+}
---
+++ APITest.Core/Controllers/LogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITest.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            //Load log file and return
+            return "foo";
+        }
+    }
+}
